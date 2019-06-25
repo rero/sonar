bb6cb6dba04300d70e03fa306eab7af936e506f0
Sébastien Délèze <sebastien.deleze@rero.ch>
2019-06-25 09:53:29 +0200

    authentication: ORCID OAuth
    NEW OAuth authentication with ORCID api.
    
    Signed-off-by: Sébastien Délèze <sebastien.deleze@rero.ch>

---
+++ sonar/modules/oauth/orcid.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 RERO.
+#
+# Swiss Open Access Repository is free software; you can redistribute it
+# and/or modify it under the terms of the MIT License; see LICENSE file for
+# more details.
+
+"""ORCID OAuth custom process for SONAR."""
+
+import orcid
+from flask import current_app
+from invenio_db import db
+from invenio_oauthclient.utils import oauth_link_external_id
+from requests import RequestException
+from slugify import slugify
+
+
+def account_info(remote, resp):
+    """Retrieve remote account information used to find local user.
+
+    It returns a dictionary with the following structure:
+
+    .. code-block:: python
+
+        {
+            'user': {
+                'profile': {
+                    'full_name': 'Full Name',
+                },
+            },
+            'external_id': 'orcid-unique-identifier',
+            'external_method': 'orcid',
+        }
+
+    :param remote: The remote application.
+    :param resp: (dict) Response given by the API.
+    :returns: A dictionary with the user information.
+    """
+    orcid_id = resp.get('orcid')
+
+    record = {
+        'external_id': orcid_id,
+        'external_method': 'orcid',
+        'user': {
+            'profile': {
+                'full_name': resp.get('name'),
+                'username': slugify(resp.get('name', '')),
+            }
+        },
+    }
+
+    # Try to get ORCID record email
+    email = get_orcid_record_email(orcid_id, resp.get('access_token'))
+
+    if email:
+        record['user']['email'] = email
+
+    return record
+
+
+def account_setup(remote, token, resp):
+    """Perform additional setup after user have been logged in.
+
+    :param remote: The remote application.
+    :param token: (str) The token value.
+    :param resp: (dict) Response returned from api.
+    """
+    # Get ORCID record detail
+    record = get_orcid_record(resp.get('orcid'), resp.get('access_token'))
+
+    with db.session.begin_nested():
+        # Retrieve ORCID from response.
+        orcid_id = resp.get('orcid')
+
+        # Set ORCID in extra_data.
+        token.remote_account.extra_data = {'orcid': orcid_id, 'record': record}
+
+        user = token.remote_account.user
+
+        # Create user <-> external id link.
+        oauth_link_external_id(user, {'id': orcid_id, 'method': 'orcid'})
+
+
+def get_orcid_record(orcid_id, token, request_type=''):
+    """Get ORCID record details.
+
+    It returns a dictionary with the following structure:
+
+    .. code-block:: python
+
+        {
+            'orcid-identifier': {
+                'path': '0000-0000-0000-0000',
+            },
+            'person': {
+                'name': {
+                    'given-names': {
+                        'value': 'John'
+                    },
+                    'family-name': {
+                        'value': 'Doe'
+                    },
+                },
+                'emails': {
+                    'email': [{
+                        'email': 'john.doe@test.com',
+                    }],
+                },
+                ...
+            },
+            ...
+        }
+
+
+    :param orcid_id: (str) ORCID identifier.
+    :param token: (str) Access token for querying API.
+    :param request_type: (str) Request type for retreiving a specific section
+    of the record.
+    :returns: (dict|None) Dictionary of the Orcid record or none if something
+    goes wrong
+    """
+    credentials = current_app.config.get('ORCID_APP_CREDENTIALS')
+    api = orcid.PublicAPI(
+        credentials['consumer_key'],
+        credentials['consumer_secret'],
+        sandbox=True,
+    )
+    try:
+        return api.read_record_public(orcid_id, request_type, token)
+    except RequestException:
+        return None
+
+
+def get_orcid_record_email(orcid_id, token):
+    """Get ORCID record email.
+
+    By default email address in ORCID is not publicly visible, so this
+    information will not be found in almost records
+
+    :param orcid_id: (str) ORCID identifier
+    :param token: (str) access token for querying API
+    :returns: (str|None) The email found or none
+    """
+    record = get_orcid_record(orcid_id, token, 'person')
+
+    if not record or 'emails' not in record or not record['emails']['email']:
+        return ''
+
+    return record['emails']['email'][0]['email']
---
+++ tests/ui/oauth/test_oauth_orcid.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 RERO.
+#
+# Swiss Open Access Repository is free software; you can redistribute it
+# and/or modify it under the terms of the MIT License; see LICENSE file for
+# more details.
+
+"""Test OAuth with ORCID."""
+
+import sonar.modules.oauth.orcid as sonar_orcid
+
+
+def test_account_info(app, mock_api_read_record, orcid_record, user_record):
+    """Test account info extraction."""
+    assert sonar_orcid.account_info(None, orcid_record) == user_record
+
+
+def test_account_setup(app, orcid_record, models_fixture,
+                       remote_account_fixture):
+    """Test account setup after login."""
+    remote_account, remote_token = remote_account_fixture
+
+    ioc = app.extensions['oauthlib.client']
+
+    sonar_orcid.account_setup(ioc.remote_apps['orcid'], remote_token,
+                              orcid_record)
+
+    assert remote_account.extra_data['orcid'] == orcid_record['orcid']
+
+
+def test_get_orcid_record(mock_api_read_record, app, orcid_record):
+    """Test to get an orcid detail."""
+    # Test valid returned record
+    record = sonar_orcid.get_orcid_record(orcid_record['orcid'],
+                                          orcid_record['access_token'])
+    assert record['orcid-identifier']['path'] == orcid_record['orcid']
+
+    # Test error when request_type is invalid
+    record = sonar_orcid.get_orcid_record(orcid_record['orcid'],
+                                          orcid_record['access_token'], 'FAKE')
+    assert not record
+
+    # Test error when access token is invalid
+    record = sonar_orcid.get_orcid_record(orcid_record['orcid'],
+                                          'NOT_EXISTING')
+    assert not record
+
+    # Test error when record id does not exist
+    record = sonar_orcid.get_orcid_record('NOT_EXISTING',
+                                          orcid_record['access_token'])
+    assert not record
+
+
+def test_get_orcid_record_email(mock_api_read_record, app, orcid_record):
+    """Test getting ORCID record email."""
+    # Test existing email
+    email = sonar_orcid.get_orcid_record_email(orcid_record['orcid'],
+                                               orcid_record['access_token'])
+    assert email == 'john.doe@test.com'
+
+    # Test when record id does not exists
+    email = sonar_orcid.get_orcid_record_email('NOT_EXISTING',
+                                               orcid_record['access_token'])
+    assert email == ''
+
+    # Test when access token does not exists
+    email = sonar_orcid.get_orcid_record_email(orcid_record['orcid'],
+                                               'NOT_EXISTING')
+    assert email == ''
