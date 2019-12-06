5fd915d65abafaf859a8c9fb0ae3fa5dbf1e8205
Sébastien Délèze <sebastien.deleze@rero.ch>
2019-12-06 14:31:35 +0100

    deposit: extract metadata from PDF
    * Extracts metadata from PDF using GROBID and stores them into metadata of the file.
    * Changes property types for journal data.
    * Installs pycountry.
    * Closes #87.
    
    Co-Authored-by: Sébastien Délèze <sebastien.deleze@rero.ch>

---
+++ tests/ui/pdf_extractor/test_pdf_extractor_utils.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+#
+# Swiss Open Access Repository
+# Copyright (C) 2019 RERO
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test PDF extractor utils."""
+
+import json
+import os
+
+from sonar.modules.pdf_extractor.utils import format_extracted_data
+
+
+def test_format_extracted_data(app):
+    """Test format extracted data."""
+    # format_extracted_data({})
+    json_file = os.path.dirname(
+        os.path.abspath(__file__)) + '/data/extracted_data.json'
+
+    with open(json_file, 'rb') as file:
+        # Test standard extraction
+        extracted_data = json.load(file)
+        formatted_data = format_extracted_data(extracted_data)
+        assert 'title' in formatted_data
+        assert formatted_data['title'] == 'Calibrated Ice Thickness Estimate' \
+            ' for All Glaciers in Austria'
+
+        # Test authors
+        extracted_data['teiHeader']['fileDesc']['sourceDesc']['biblStruct'][
+            'analytic']['author'] = extracted_data['teiHeader']['fileDesc'][
+                'sourceDesc']['biblStruct']['analytic']['author'][0]
+
+        formatted_data = format_extracted_data(extracted_data)
+        assert len(formatted_data['authors']) == 1
+
+        # Test languages
+        extracted_data['text']['@xml:lang'] = 'de'
+        formatted_data = format_extracted_data(extracted_data)
+        assert formatted_data['languages'][0] == 'ger'
+
+        # Test imprint
+        extracted_data['teiHeader']['fileDesc']['sourceDesc']['biblStruct'][
+            'monogr']['imprint']['biblScope'] = extracted_data['teiHeader'][
+                'fileDesc']['sourceDesc']['biblStruct']['monogr']['imprint'][
+                    'biblScope'][0]
+        formatted_data = format_extracted_data(extracted_data)
+        assert formatted_data['journal']['name'] == 'Frontiers Media SA'
+        assert formatted_data['journal']['volume'] == '7'
