cadb867a448d2a54928b597d479cd8ea3faca2ff
Pascal Repond <pascal.repond@rero.ch>
2025-08-05 15:20:51 +0200

    chore: replace poetry by uv
    Updates dependencies and use uv as dependency manager.
    
    Co-Authored-by: Pascal Repond <pascal.repond@rero.ch>

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -16,15 +16,15 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-lock_file="$(dirname "$0")/../poetry.lock"
+lock_file="$(dirname "$0")/../uv.lock"
 
 
 if [ ! -f $lock_file ]; then
-    echo "Lock file not found. Generate it by running 'poetry lock'."
+    echo "Lock file not found. Generate it by running 'uv sync'."
     exit 1
 fi
 
-# Extract poetry.lock hash to use as the docker image tag
+# Extract uv.lock hash to use as the docker image tag
 deps_ver="$(sed -n 's/content-hash = "\(.*\)"/\1/p' $lock_file)"
 
 # Build dependencies image
