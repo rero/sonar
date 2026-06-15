52adc13fa46ff7227207c771c096e034796a5163
Pascal Repond <pascal.repond@rero.ch>
2026-06-15 15:47:11 +0200

    style: replace verbose license headers with SPDX tags
    Co-Authored-by: Pascal Repond <pascal.repond@rero.ch>

--- sonar/modules/users/mappings/v7/__init__.py
+++ sonar/modules/users/mappings/v7/__init__.py
@@ -1,6 +1,4 @@
-# Copyright (C) 2019 CERN.
-#
-# My site is free software; you can redistribute it and/or modify it under
-# the terms of the MIT License; see LICENSE file for more details.
+# SPDX-FileCopyrightText: CERN
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 """Mappings for Elasticsearch 6.x."""
